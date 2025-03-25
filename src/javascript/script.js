454c3ea349eb29e2b918dadbdd6ba5a626b0a7e3
ixsilva <ixsilva@sfiec.org.br>
2025-03-24 22:55:30 -0300

    atualizacao24032025

---
+++ src/javascript/script.js
@@ -0,0 +1,6 @@
+jQuery(function () {  
+    $('#mobile_btn').on('click', function(){
+        $('#mobile_menu').toggleClass('active')
+        $('#mobile_btn').find('i').toggleClass('fa-x')
+    }); 
+});
